a30478ef1cdb8d4eea5b9c43401d0c89e7271e27
josuem317 <jowebdesigc@gmail.com>
2024-08-24 01:03:13 -0400

    p

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,19 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				iconAnimation: {
+					'0%': { transform: 'scale(1)' },
+					'50%': { transform: 'scale(1)' },
+					'100%': { transform: 'scale(1)' },
+				},
+			},
+			animation: {
+				iconAnimation: 'iconAnimation 2s infinite',
+			},
+		},
 	},
 	plugins: [],
+
 }
